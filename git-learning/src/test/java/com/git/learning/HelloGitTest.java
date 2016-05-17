a81fda4b6123c270e38440cf1058b5c727c28f20
dungtp <dungtp.vnu@gmail.com>
2016-05-17 15:06:27 +0700

    intial git-learing project

---
+++ git-learning/src/test/java/com/git/learning/HelloGitTest.java
@@ -0,0 +1,19 @@
+package com.git.learning;
+
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class HelloGitTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
